25fe2920e4f3eee5f746969e6c842790edaa7ece
JonArntK <54376106+JonArntK@users.noreply.github.com>
2022-02-08 18:39:28 +0100

    Finalized CameraFollow script

--- Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -4,42 +4,49 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float smoothness;
+    [Range(0f, 10f)] public float smoothness;
     public Transform targetObject;
 
-    private Vector3 initialTransformOffset;
-    private float initialRotationYOffset, radiusOffset;
-
-    private Vector3 cameraPosition;
-    private float cameraRotationY;
+    private Vector3 initialTransformOffset, initialRotationOffset;
+    private float radiusOffset;
 
     void Start()
     {
+        // Compute intial transform offset (including radius in xz-plane), rotation offset.
         initialTransformOffset = transform.position - targetObject.position;
-        initialRotationYOffset = transform.localEulerAngles.y - targetObject.localEulerAngles.y;
+        initialRotationOffset = transform.localEulerAngles - targetObject.localEulerAngles;
         radiusOffset = Mathf.Sqrt(initialTransformOffset.x * initialTransformOffset.x + initialTransformOffset.z * initialTransformOffset.z);
     }
 
     void FixedUpdate()
     {
-        // Move camera with target.
-        //cameraPosition = targetObject.position + initialTransformOffset;
-        float dx = radiusOffset * Mathf.Cos(targetObject.localEulerAngles.y - initialRotationYOffset);
-        float dz = radiusOffset * Mathf.Sin(targetObject.localEulerAngles.y - initialRotationYOffset);
-        Debug.Log(dx + ":" + dz + "InitialRotationYOffset = " + initialRotationYOffset);
-        cameraPosition = targetObject.position + new Vector3(dx, initialTransformOffset.y, dz);
-        //transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothness * Time.fixedDeltaTime);
+        RotateCamera();
+        MoveCamera();
+    }
 
-
+    private void RotateCamera()
+    {
         // Rotate camera with target.
-        cameraRotationY = targetObject.localEulerAngles.y + initialRotationYOffset;
+        float cameraRotationY = targetObject.localEulerAngles.y + initialRotationOffset.y;
 
         // Rotate the cube by converting the angles into a quaternion.
-        Quaternion target = Quaternion.Euler(0, cameraRotationY, 0);
+        Quaternion target = Quaternion.Euler(initialRotationOffset.x, cameraRotationY, initialRotationOffset.z);
 
-        // Dampen towards the target rotation
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smoothness);
+        // Dampen towards the target rotation.
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smoothness);
+    }
 
-        transform.RotateAround(targetObject.position, Vector3.up, 10 * Time.deltaTime);
+    private void MoveCamera()
+    {
+        // Move camera with target.
+        float angle = (targetObject.localEulerAngles.y + initialRotationOffset.y - 90f) / 360f * 2f * Mathf.PI;
+        float dx = radiusOffset * -Mathf.Cos(angle);
+        float dz = radiusOffset * Mathf.Sin(angle);
+
+        // Compute new position based on updated position and rotation of target.
+        Vector3 cameraPosition = targetObject.position + new Vector3(dx, initialTransformOffset.y, dz);
+
+        // Dampen towards the target position.
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothness * Time.fixedDeltaTime);
     }
 }
