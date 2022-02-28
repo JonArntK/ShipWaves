2db3403d401bbd4fb998550f0677b5fc390840ee
JonArntK <54376106+JonArntK@users.noreply.github.com>
2022-02-28 15:23:20 +0100

    StoreTriangle()

--- Assets/Scripts/GenerateMesh.hlsl
+++ Assets/Scripts/GenerateMesh.hlsl
@@ -23,4 +23,35 @@ struct Vertex
     float2 texcoord;
 };
 
+void StoreTriangle(uint idx1, uint idx2, uint idx3, Vertex v1, Vertex v2, Vertex v3, RWByteAddressBuffer _VertexBuffer, RWByteAddressBuffer _NormalBuffer, RWByteAddressBuffer _TexcoordBuffer)
+{
+    // Triangle vertices (v1, v2, v3) should be given in a counter clock-wise direction.
+
+    float3 p1 = v1.position;
+    float3 p2 = v2.position;
+    float3 p3 = v3.position;
+
+    float2 uv1 = v1.texcoord;
+    float2 uv2 = v2.texcoord;
+    float2 uv3 = v3.texcoord;
+
+    // Compute normal vector.
+    float3 normal = -normalize(cross(p2 - p1, p3 - p2));
+
+    // Store vertex position.
+    Store3(_VertexBuffer, idx1, p1);
+    Store3(_VertexBuffer, idx2, p2);
+    Store3(_VertexBuffer, idx3, p3);
+
+    // Store vertex texture coordinate.
+    Store2(_TexcoordBuffer, idx1, uv1);
+    Store2(_TexcoordBuffer, idx2, uv2);
+    Store2(_TexcoordBuffer, idx3, uv3);
+
+    // Store vertex normal vector.
+    Store3(_NormalBuffer, idx1, normal);
+    Store3(_NormalBuffer, idx2, normal);
+    Store3(_NormalBuffer, idx3, normal);
+}
+
 #endif // __GENERATEMESH_HLSL__
